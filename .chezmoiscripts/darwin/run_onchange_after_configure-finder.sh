22872086d188802cb6e32683b6f6df09f09df31b
Clément Cassé <clement.casse@proton.me>
2026-02-15 17:20:16 +0100

    enhance macos customization and reading the doc so that i do not install rust complete

--- .chezmoiscripts/darwin/run_onchange_after_configure-finder.sh
+++ .chezmoiscripts/darwin/run_onchange_after_configure-finder.sh
@@ -4,9 +4,20 @@ set -eufo pipefail
 
 trap 'killall Finder' EXIT
 
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+
 defaults write com.apple.finder FXPreferredViewStyle -string clmv
 defaults write com.apple.finder _FXSortFoldersFirst -int 1
 defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -int 1
 defaults write com.apple.finder ShowPathbar -int 1
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -int 1
+defaults write com.apple.finder ShowStatusBar -int 0
+defaults write com.apple.finder AppleShowAllFiles -int 0
+defaults write com.apple.finder FXRemoveOldTrashItems -int 1
+
 defaults write com.apple.finder ShowHardDrivesOnDesktop -int 1
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -int 1
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -int 1
+defaults write com.apple.finder ShowMountedServersOnDesktop -int 1
+
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -int 1
+defaults write com.apple.desktopservices DSDontWriteUSBStores -int 1
